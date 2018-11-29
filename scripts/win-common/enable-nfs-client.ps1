06d780c6a27557854baca99d22e67269ea6dd2f6
Leonard Lee <leonard.lee@praqma.com>
2018-11-29 13:21:45 +0100

    Fix typographical error.

--- scripts/win-common/enable-nfs-client.ps1
+++ scripts/win-common/enable-nfs-client.ps1
@@ -4,6 +4,6 @@ $ErrorActionPreference = "Stop" # Fail fast!
 
 Get-WindowsOptionalFeature -Online -Verbose | findstr /i nfs
 
-Enable-WindowsOptionalFeature -Online -FeatureName "ServiceForNFS-ClientOnly" -All -Verbose
+Enable-WindowsOptionalFeature -Online -FeatureName "ServicesForNFS-ClientOnly" -All -Verbose
 Enable-WindowsOptionalFeature -Online -FeatureName "NFS-Administration" -All -Verbose
 Enable-WindowsOptionalFeature -Online -FeatureName "ClientForNFS-Infrastructure" -All -Verbose
